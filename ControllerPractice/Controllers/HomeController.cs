ecbe3d7045f26ee41edfd4927b8d7471a0a4847d
AJ Willsea <AJWillsea@rocketmortgage.com>
2022-09-20 16:57:26 -0400

    commit

---
+++ ControllerPractice/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ControllerPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ControllerPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+        public IActionResult Samplelogin(string firstname,string lastname,string email,string password)
+        {
+            ViewBag.Firstname = firstname;
+            ViewBag.Lastname = lastname;
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MYTEST()
+        {
+            return Content("Hello World");
+        }
+        public IActionResult CurrentTime()
+        {
+            Request.ContentType = "application/text/html";
+            DateTime current = DateTime.Now;
+            return Content($"The Current<b> Date<b> and time is {current.ToString()}");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(string username, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Password = password;
+            return View();
+            //return Content($"{username} {password}");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
